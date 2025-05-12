9ae00952cd5919219ab8689952fb816b8d94da11
Chatbot-creator <amirshayan.m@gmail.com>
2025-05-12 22:34:55 +0330

    updated_Backend

--- Backend/App/properties/routes.py
+++ Backend/App/properties/routes.py
@@ -956,7 +956,7 @@ def recursive_json_decode(data):
 #         "total": len(filtered)
 #     }
 
-
+# filter
 @router.post("/filter")
 async def filter_properties_route(
     filter_params: FilterParams,
